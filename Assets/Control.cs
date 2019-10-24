9dda9257d1c4d3ab14cb9006546220be34915bb9
Duy Do <juchiast@gmail.com>
2019-10-24 07:55:42 -0700

    init

---
+++ Assets/Control.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    Animator animator;
+    SpriteRenderer ren;
+    Rigidbody2D body;
+    int walking = 0;
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        ren = GetComponent<SpriteRenderer>();
+        body = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.ResetTrigger("triggerJump");
+        if (Input.GetKeyDown(KeyCode.W)) {
+            animator.SetTrigger("triggerJump");
+        }
+        if (Input.GetKeyDown(KeyCode.A)) {
+            walking = -1;
+        }
+        if (Input.GetKeyUp(KeyCode.A)) {
+            walking = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {
+            walking = 1;
+        }
+        if (Input.GetKeyUp(KeyCode.D)) {
+            walking = 0;
+        }
+
+        if (walking == 1) {
+            ren.flipX = false;
+        }
+        else if (walking == -1) {
+            ren.flipX = true;
+        }
+
+        animator.SetInteger("walking", walking);
+
+        doCamera();
+    }
+
+    void doCamera() {
+        Vector3 pos = cam.transform.position;
+        pos.x = transform.position.x;
+        pos.y = transform.position.y;
+        pos.y = Mathf.Max(pos.y, 0f);
+        pos.x = Mathf.Max(pos.x, -11.42f);
+        pos.x = Mathf.Min(pos.x, 22.58f);
+
+        cam.transform.position = pos;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        animator.SetTrigger("triggerIdle");
+        // walking = 0;
+    }
+}
---
+++ Assets/PetControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetControl : MonoBehaviour
+{
+    Animator animator;
+    SpriteRenderer ren;
+    int state = 0;
+    int count = 0;
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+        ren = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        count += 1;
+        if (count / 40 % 2 == 0) {
+            ren.flipX = true;
+        } else {
+            ren.flipX = false;
+        }
+        if (count % 30 == 0) {
+            state = 2;
+        } else {
+            state = 1;
+        }
+        animator.SetInteger("pet_state", state);
+        if (animator.GetCurrentAnimatorStateInfo(animator.GetLayerIndex("default")).IsName("pet-walk")) {
+            int d = 0;
+            if (ren.flipX) d = -1;
+            else d = 1;
+            Vector2 pos = transform.position;
+            pos.x += d * 0.06f;
+            transform.position = pos;
+        }
+    }
+}
